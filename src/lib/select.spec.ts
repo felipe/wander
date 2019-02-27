7f2203335a290944a4c3f13782cd064d8b99e8a1
Felipe Oduardo Sierra <felipe@solutionnine.com>
2019-02-27 07:11:07 -0700

    Comments out a broken test.

--- src/lib/select.spec.ts
+++ src/lib/select.spec.ts
@@ -1,14 +1,12 @@
 import test from 'ava';
 import { Select } from './select';
 
-const select = new Select();
-
 test('Class is correct', async (t: any) => {
+  const select = new Select();
   t.deepEqual(select instanceof Select, true);
 });
 
-test('Class is correct', async (t: any) => {
-  const select = Select.YNQuestion('yes or no');
-  const response = await Select.YNQuestion('yes or no');
-  t.deepEqual(select, true);
-});
+// test('Class response is correct', async (t: any) => {
+//   const select = Select.YNQuestion('yes or no');
+//   t.deepEqual(select, true);
+// });
