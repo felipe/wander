00b2738ade2b2cc4ce36fe5ce2863f49e7495575
Felipe Oduardo Sierra <felipe@solutionnine.com>
2019-03-05 22:07:40 -0700

    Ignores items in Select since testing `readlineSync` has been a challenge.

--- src/lib/select.spec.ts
+++ src/lib/select.spec.ts
@@ -5,8 +5,3 @@ test('Class is correct', async (t: any) => {
   const select = new Select();
   t.deepEqual(select instanceof Select, true);
 });
-
-// test('Class response is correct', async (t: any) => {
-//   const select = Select.YNQuestion('yes or no');
-//   t.deepEqual(select, true);
-// });
